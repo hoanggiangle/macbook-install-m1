b8c8f068f75e48ef89918d61648018831781d87e
gianglh <hoanggiangco94@gmail.com>
2021-03-03 10:26:13 +0700

    install for dev

---
+++ README.md
@@ -0,0 +1,12 @@
+# macbook-setup
+Script to setup my M1 Macbook. Optimized for Golang software engineeer.
+
+Usage:
+
+```sudo /bin/bash -c ./setup.sh"```
+
+To upgrade all softwares installed by cask later, just run this below command:
+
+```brew upgrade --cask --greedy```
+
+To install Docker, you will need to manually download the Tech Preview version at: https://docs.docker.com/docker-for-mac/apple-m1/
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+echo Installing Rosetta...
+sudo softwareupdate --install-rosetta
+
+echo Installing Homebrew...
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+export PATH=/opt/homebrew/bin:$PATH
+brew update
+
+echo Installing basic homebrew softwares...
+brew install git php ruby node npm protobuf redis mysql rabbitmq mercurial wget zsh htop go
+
+# apps
+echo Installing desktop apps...
+brew install --cask google-chrome iterm2 folx dropbox adobe-acrobat-reader ledger-live textmate skype slack firefox vlc zoom viber microsoft-edge openemu anydesk microsoft-office steam
+# utils
+echo Installing utilities...
+brew install --cask little-snitch istat-menus clipy grandperspective ipartition spectacle vipriser cd-to blackhole-2ch osxfuse handbrake the-unarchiver textbar adguard android-file-transfer balenaetcher sidequest
+# dev tools
+echo Installing dev tools...
+brew install --cask oracle-jdk docker sequel-ace visual-studio-code goland phpstorm tunnelblick dosbox-x postman wireshark altserver ifunbox sourcetree expandrive proxyman  brew install android-platform-tools
+
+# default-writes
+echo Setting default-writes...
+sudo /bin/bash -c ./default-writes.sh
+
+cd Downloads
+curl -O https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
+
+# oh-my-zsh
+echo Installing Oh-my-zsh...
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+echo Next step: Copy dotfiles to home directory
+
+# To upgrade all softwares installed by cask later, just run this below command:
+# brew upgrade --cask --greedy
